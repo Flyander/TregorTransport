e50c11e86de40ae355785a7273b2ab74a1842338
YohannFin <yohann.fin13@gmail.com>
2019-10-08 14:19:30 +0200

    Ajoutez des fichiers projet.

---
+++ TregorTransport/TregorTransport/VuesModeles/PrincipalVueModele.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace TregorTransport.VuesModeles
+{
+    class PrincipalVueModele : INotifyPropertyChanged
+    {
+        #region Attributs
+        #endregion
+
+        #region Constructeurs
+        #endregion
+
+        #region Getters/Setters
+        #endregion
+
+        #region Methodes
+        #endregion
+
+        #region Notification
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+    }
+}
