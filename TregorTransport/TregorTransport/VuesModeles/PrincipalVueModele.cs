1b76d8278c5f620b6f79ac27cf607fda0071feb8
YohannFin <yohann.fin13@gmail.com>
2019-11-12 15:08:23 +0100

    Last version of login page, add hyperlink.

--- TregorTransport/TregorTransport/VuesModeles/PrincipalVueModele.cs
+++ TregorTransport/TregorTransport/VuesModeles/PrincipalVueModele.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Diagnostics;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Windows.Input;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 
 namespace TregorTransport.VuesModeles
@@ -20,6 +22,41 @@ namespace TregorTransport.VuesModeles
         #endregion
 
         #region Methodes
+        private Boolean IsValidUri(String uri)
+        {
+            try
+            {
+                new Uri(uri);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public ICommand ClickCommand => new Command<string>(async (url) =>
+        {
+            try
+            {
+                if (!string.IsNullOrEmpty(url))
+                {
+                    if (!url.Trim().StartsWith("http", StringComparison.OrdinalIgnoreCase))
+                    {
+                        url = "http://" + url;
+                    }
+                    if (IsValidUri(url))
+                    {
+                        await Browser.OpenAsync(new Uri(url), BrowserLaunchMode.SystemPreferred);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+
+        });
         #endregion
 
         #region Notification
